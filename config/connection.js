b18f307eebe2fdf2e3bf6b4c33a66c1e3764f033
J Fav <jfavorito@outlook.com>
2020-06-02 17:58:58 -0700

    added JAWS connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 const mysql = require("mysql");
 const util = require("util");
+let connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+}
 
 connection.connect(function (error) {
     if (error) {
